f85fc620178cccdbb4c862f0bddacf31c301f113
azat <azat5618@gmail.com>
2026-04-30 22:59:09 +0500

    added main entities

---
+++ backend-spring/Category/Category.java
@@ -0,0 +1,26 @@
+package Category;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
---
+++ backend-spring/Product/Product.java
@@ -0,0 +1,28 @@
+package Product;
+
+import Category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private String image;
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
