30e37d75b980be3d2090017e4e78dd752259cc18
azat <azat5618@gmail.com>
2026-05-01 16:04:40 +0500

    added Controllers and Repositories

---
+++ backend-spring/Controllers/OrderController.java
@@ -0,0 +1,41 @@
+package Controllers;
+
+import Order.*;
+import Repositories.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping
+    public List<Order> getOrders(@RequestParam(required = false) Long userId) {
+        if (userId != null) {
+            return orderRepository.findByUserId(userId);
+        }
+
+        return orderRepository.findAll();
+    }
+
+    @PostMapping
+    public Order addOrder(@RequestBody Order order) {
+        if (order.getItems() != null) {
+            for (OrderItem item : order.getItems()) {
+                item.setOrder(order);
+            }
+        }
+
+        return orderRepository.save(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderRepository.deleteById(id);
+    }
+}
